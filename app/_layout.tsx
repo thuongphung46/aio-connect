63a9bab93857b5b030df9ec9260ec3a4510ed247
Phung Hoai Thuong <thuongphung46@gmail.com>
2023-11-29 22:40:33 +0700

    init project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,23 @@
+import { Provider } from "react-redux";
+import { store } from "~/src/redux/store";
+import { App } from "./app";
 import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    />
+    <Provider store={store}>
+      <App></App>
+    </Provider>
+    // <Stack
+    //   screenOptions={{
+    //     headerStyle: {
+    //       backgroundColor: "#f4511e",
+    //     },
+    //     headerTintColor: "#fff",
+    //     headerTitleStyle: {
+    //       fontWeight: "bold",
+    //     },
+    //   }}
+    // />
   );
 }
