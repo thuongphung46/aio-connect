a3293b8cf00acd17c7953d9b50d396555718c400
thuongphung <thuongphung46@gmail.com>
2023-12-03 15:42:44 +0700

    test commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,11 @@
-import { Provider } from "react-redux";
-import { store } from "~/src/redux/store";
-import { App } from "./app";
-import { Stack } from "expo-router";
+import type { ReactNode } from "react";
+import { DoobooProvider } from "dooboo-ui";
+import { Slot } from "expo-router";
 
-export default function Layout() {
+export default function RootLayout(): ReactNode {
   return (
-    <Provider store={store}>
-      <App></App>
-    </Provider>
-    // <Stack
-    //   screenOptions={{
-    //     headerStyle: {
-    //       backgroundColor: "#f4511e",
-    //     },
-    //     headerTintColor: "#fff",
-    //     headerTitleStyle: {
-    //       fontWeight: "bold",
-    //     },
-    //   }}
-    // />
+    <DoobooProvider>
+      <Slot />
+    </DoobooProvider>
   );
 }
